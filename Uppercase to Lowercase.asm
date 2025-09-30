4da326feac7308030e651bfc9aeaec0ae05d2472
Siyam Mia <siyambd188@gmail.com>
2025-09-30 21:27:51 +0600

    Create Uppercase to Lowercase.asm

---
+++ Uppercase to Lowercase.asm
@@ -0,0 +1,20 @@
+.model small
+.stack 100h
+.code
+main proc
+    
+    mov ah,1
+    int 21h
+    mov bl,al   
+    
+    add bl,32
+    
+    mov ah,2
+    mov dl,bl
+    int 21h   
+    
+    
+    
+   
+    main endp
+end main
