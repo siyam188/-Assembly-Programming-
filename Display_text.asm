2f4a621b856e6eb7641f972d0a015def27d3f7b3
Siyam Mia <siyambd188@gmail.com>
2025-09-30 20:45:12 +0600

    Create Display_text.asm

---
+++ Display_text.asm
@@ -0,0 +1,18 @@
+.model small
+.stack 100h
+.data
+a db 'I Love Bangleades $'
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
