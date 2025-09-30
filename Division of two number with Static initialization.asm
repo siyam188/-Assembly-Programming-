7fe215dc2dd11b924748e83e905199e8a4bd38c6
Siyam Mia <siyambd188@gmail.com>
2025-09-30 21:44:01 +0600

    Create Division of two number with Static initialization.asm

---
+++ Division of two number with Static initialization.asm
@@ -0,0 +1,25 @@
+.model small
+.stack 100h  
+.code
+main proc  
+     
+   mov al,8
+   mov bl,3
+   div bl
+            
+   mov cl,al
+   mov ch,ah     
+            
+   
+    mov ah,2
+    mov dl,cl 
+    add dl,48
+    int 21h  
+             
+    mov ah,2
+    mov dl,ch
+    add dl,48
+    int 21h 
+   
+    main endp
+end main
