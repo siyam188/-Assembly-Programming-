1450f2bbe5d7af188d6e24e34653210a8de8dbf4
Siyam Mia <siyambd188@gmail.com>
2025-09-30 21:31:39 +0600

    Create Hex digit to decimal conversion.asm

---
+++ Hex digit to decimal conversion.asm
@@ -0,0 +1,29 @@
+.model small
+.stack 100h
+.code
+main proc
+    
+    mov ah,1
+    int 21h
+    mov bl,al   
+    
+    sub bl,17
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    
+    mov ah,2
+    mov dl,49
+    int 21h  
+    
+    mov ah,2
+    mov dl,bl
+    int 21h   
+    
+    
+    
+   
+    main endp
+end main
